a0fb9c0f15c78c1e601f573a16047a7b2fdb8003
Brian Rush and Julian Flood <brian+julian@>
2017-06-26 13:27:51 -0700

    add prototypes to count vowels and consonants and, in the front end, display the entered title, text, and vowel and consonant totals

--- js/journal.js
+++ js/journal.js
@@ -8,22 +8,33 @@ Entry.prototype.wordCount = function(body) {
   return count;
 }
 
-//Prototype to create array of all letters
-Entry.prototype.letterArray = function(body) {
+Entry.prototype.countVowels = function(body) {
   var allLetters = body.toLowerCase().split('');
   var vowels = ["a", "e", "i", "o", "u"]
-  var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
   var vowelsArray = [];
-  var consonantsArray = [];
   for (i = 0; i < allLetters.length; i++) {
     if (vowels.includes(allLetters[i])) {
       vowelsArray.push(allLetters[i]);
-    } else if (consonants.includes(allLetters[i])) {
+    }
+  }
+  var vowelTotal = vowelsArray.length;
+  console.log(vowelTotal);
+  return vowelTotal;
+}
+
+Entry.prototype.countConsonants = function(body) {
+  var allLetters = body.toLowerCase().split('');
+  var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
+  var consonantsArray = [];
+  for (i = 0; i < allLetters.length; i++) {
+    if (consonants.includes(allLetters[i])) {
       consonantsArray.push(allLetters[i]);
     }
   }
 
-  console.log(vowelsArray);
+  var consonantsTotal = consonantsArray.length;
+  console.log(consonantsTotal);
+  return consonantsTotal;
 }
 
 //Front End
@@ -35,7 +46,9 @@ $(document).ready(function() {
     var newEntry = new Entry(title,body)
     console.log(newEntry);
     console.log(newEntry.wordCount(body));
-    newEntry.letterArray(body);
+    var vowelCount = newEntry.countVowels(body);
+    var consonantCount = newEntry.countConsonants(body);
 
+    $('#output').append('<h3>' + title + '</h3><p>' + body + '</p><h3>' + "The total of vowels used in the body is " + vowelCount + '</h3><h3>' + "The total of consonants used in the body is " + consonantCount + '</h3>');
   });
 });
