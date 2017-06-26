2eb80a0ef4b5ea9eb6ccd35be68201393b9c9686
Brian Rush and Julian Flood <brian+julian@>
2017-06-26 13:30:27 -0700

    add output of word count

--- js/journal.js
+++ js/journal.js
@@ -45,10 +45,10 @@ $(document).ready(function() {
     var body = $('#body').val();
     var newEntry = new Entry(title,body)
     console.log(newEntry);
-    console.log(newEntry.wordCount(body));
+    var wordTotal = newEntry.wordCount(body);
     var vowelCount = newEntry.countVowels(body);
     var consonantCount = newEntry.countConsonants(body);
 
-    $('#output').append('<h3>' + title + '</h3><p>' + body + '</p><h3>' + "The total of vowels used in the body is " + vowelCount + '</h3><h3>' + "The total of consonants used in the body is " + consonantCount + '</h3>');
+    $('#output').append('<h3>' + title + '</h3><p>' + body + '</p><h3>' + "Word count: " + wordTotal + '</h3><h3>' + "The total of vowels used in the body is " + vowelCount + '</h3><h3>' + "The total of consonants used in the body is " + consonantCount + '</h3>');
   });
 });
